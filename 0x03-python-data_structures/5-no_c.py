9b047175864af010cf3a5e4fc36ffa7732378221
EsayasA <esayasaregawi29@gmail.com>
2023-10-08 08:44:59 -0700

    number5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 
 
 def no_c(my_string):
-    dup = [y for y in my_string if y != 'c' and y != 'C']
-    return ("".join(dup))
+    rep = [y for y in my_string if y != 'c' and y != 'C']
+    return ("".join(rep))
 
