02b40353d4a317c6a812df1d42cea7bd13bcaf53
EsayasA <esayasaregawi29@gmail.com>
2023-10-09 00:09:52 -0700

    number10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,13 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
 
 def divisible_by_2(my_list=[]):
-    product = []
-    for k in range(len(my_list)):
-        if my_list[k] % 2 == 0:
-            product.append(True)
+    products = []
+    for j in range(len(my_list)):
+        if my_list[j] % 2 == 0:
+            products.append(True)
         else:
-            product.append(False)
+            products.append(False)
 
-    return (product)
+    return (products)
+
