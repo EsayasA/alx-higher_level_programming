bbeadb31a3ed23a426ca016a660f8a2ef9971690
EsayasA <esayasaregawi29@gmail.com>
2023-10-25 06:21:41 -0700

    number1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class."""
+
+
+class Square:
+    """Define a class of square."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: size of square.
+        """
+        self.__size = size
+
