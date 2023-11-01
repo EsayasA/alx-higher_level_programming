fcb520804b3886ee9e20fbe8424f67998129280a
EsayasA <esayasaregawi29@gmail.com>
2023-11-01 15:49:22 -0700

    rec

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+1-rectangle.py
+#!/usr/bin/python3
+"""Defines the class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize Rectangle.
+
+        Args:
+            width (int): width.
+            height (int): height.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
