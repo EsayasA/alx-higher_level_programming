2d09b0192769fe61204b74d00708a4cb99b459bf
EsayasA <esayasaregawi29@gmail.com>
2024-01-25 08:52:52 -0800

    number 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # script take and send url, and displays
-# the size body of the response.
-# The size in bytes
-# You use curl
-# script that takes , sends a request to that URl and display.
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
