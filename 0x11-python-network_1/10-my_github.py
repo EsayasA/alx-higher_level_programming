aa3c0167ea08a156bb412a700b437b30798c12e0
EsayasA <esayasaregawi29@gmail.com>
2024-01-27 22:30:01 -0800

    number 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Sends a search request for a given string to the Star Wars APi"""
+"""Sends a search request for a given string to the Star Wars API.
+
+Usage: ./9-starwars.py <search string>
+  - The search request is sent to the Star Wars API search people endpoint.
+"""
 import sys
 import requests
 
 
 if __name__ == "__main__":
     url = "https://swapi.co/api/people"
-    par = {"search": sys.argv[1]}
-    res = requests.get(url, par=par).json()
+    params = {"search": sys.argv[1]}
+    results = requests.get(url, params=params).json()
 
-    print("Number of results: {}".format(res.get("count")))
-    [print(req.get("name")) for req in res.get("res")]
+    print("Number of results: {}".format(results.get("count")))
+    [print(r.get("name")) for r in results.get("results")]
