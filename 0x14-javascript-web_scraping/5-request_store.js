4446651a1c63205168073b5dfd0491eeef126ec1
EsayasA <esayasaregawi29@gmail.com>
2024-02-20 04:07:13 -0800

    task5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
-const req = require('req');
-req(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
