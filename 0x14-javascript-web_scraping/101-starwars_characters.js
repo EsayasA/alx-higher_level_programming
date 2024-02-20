ab555ca143a384154647b4e6fdf32e1c810a4fd7
EsayasA <esayasaregawi29@gmail.com>
2024-02-20 05:56:29 -0800

    task8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error1, ans, body) {
+  if (!error1) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, j) {
+  request(characters[i], function (error1, ans, body) {
+    if (!error1) {
+      console.log(JSON.parse(body).name);
+      if (j + 1 < characters.length) {
+        printCharacters(characters, j + 1);
+      }
+    }
+  });
+}
