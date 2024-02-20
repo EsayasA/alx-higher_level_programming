67cf4487be5908cbabdab01ae03fd044fa420504
EsayasA <esayasaregawi29@gmail.com>
2024-02-20 04:26:27 -0800

    task6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const req = require('req');
+req(process.argv[2], function (err, res, body) {
+  if (!err) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
+});
